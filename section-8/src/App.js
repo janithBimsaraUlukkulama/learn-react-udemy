3e3765532a350306dd823d92f24e4564e248421e
bimsara <jbimsaraulukkulama@outlook.com>
2022-07-07 08:19:25 +0530

    section-8 Adding UserList component

--- section-8/src/App.js
+++ section-8/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import AddUser from "./componenents/Users/AddUser";
-
+import UsersList from "./componenents/Users/UsersList";
 
 function App() {
     return (
         <div>
             <AddUser/>
+            <UsersList users={[]}/>
         </div>
     );
 }
