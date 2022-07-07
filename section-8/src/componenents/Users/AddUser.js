7cb430da11b5fcc0dfde2c9df733b475847ab6b2
bimsara <jbimsaraulukkulama@outlook.com>
2022-07-07 07:45:28 +0530

    section-8 Add user re-usable "Card" component

--- section-8/src/componenents/Users/AddUser.js
+++ section-8/src/componenents/Users/AddUser.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = props => {
-    const addUserHandler = (event)=>{
+    const addUserHandler = (event) => {
         event.preventDefault(); // prevent default form on submit
     }
 
-    return <form onSubmit={addUserHandler}>
-        <label htmlFor="userName">Username</label>
-        <input id="userName" type="text"/>
-        <label htmlFor="age">Username</label>
-        <input id="age" type="number"/>
-        <button type="submit">Add User</button>
-    </form>
+    return (
+        <Card className={classes.input} >
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="userName">Username</label>
+                <input id="userName" type="text"/>
+                <label htmlFor="age">Username</label>
+                <input id="age" type="number"/>
+                <button type="submit">Add User</button>
+            </form>
+        </Card>)
 };
 
 export default AddUser;
