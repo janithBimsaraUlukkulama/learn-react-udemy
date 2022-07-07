a80aff63788186b9e2d34f712944c3736799ddba
bimsara <jbimsaraulukkulama@outlook.com>
2022-07-07 07:52:20 +0530

    section-8 Add user re-usable "Button" component

--- section-8/src/componenents/Users/AddUser.js
+++ section-8/src/componenents/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 
 const AddUser = props => {
     const addUserHandler = (event) => {
@@ -8,13 +9,13 @@ const AddUser = props => {
     }
 
     return (
-        <Card className={classes.input} >
+        <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="userName">Username</label>
                 <input id="userName" type="text"/>
-                <label htmlFor="age">Username</label>
+                <label htmlFor="age">Age (Years)</label>
                 <input id="age" type="number"/>
-                <button type="submit">Add User</button>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>)
 };
